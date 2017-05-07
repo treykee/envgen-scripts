0a8d2b5b1e33d13868c732ea628783188bd0e50d
Trey Keenon <treykee@gmail.com>
2017-05-07 00:56:22 -0500

    Update regex for config update

--- install_pgsql.sh
+++ install_pgsql.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Installs and configures 
 
 if [[ $EUID -ne 0 ]];
 then
@@ -10,7 +11,16 @@ else
 
     # Install and configure PostgreSQL
     yum install -y postgresql-server postgresql-contrib
+    
+    # Create a new PostgreSQL database cluster
     postgresql-setup initdb
+    
+    # Create backup and modify pg_hba.conf to allow password authentication for PostgreSQL
     cp $pg_hba_conf /var/lib/pgsql/data/pg_hba.conf.original
-    sed -i -e '(host)[ \t]+(all)[ \t]+(all)[ \t]+(\b(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}\b\/\d{1,2})[ \t]+(ident)/s/ident/md5/g' $pg_hba_conf
-    sed -i -e '(host)[ \t]+(all)[ \t]+(all)[ \t]+(::1\/\d{1,3})[ \t]+(ident)/s/ident/md5/g' $pg_hba_conf
+    sed -i -e '\(host\)[ \t]+(all)[ \t]+(all)[ \t]+(\b(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}\b\/\d{1,2})[ \t]+(ident)/s/ident/md5/g' $pg_hba_conf
+    sed -i -e '\(host\)[ \t]+\(all\)[ \t]+\(all\)[ \t]+\(::1\/\d{1,3}\)[ \t]+\(ident\)/s/ident/md5/g' $pg_hba_conf
+    
+    # Start and enable PostgreSQL
+    systemctl start postgresql
+    systemctl enable postgresql
+fi
